5235e1b2097faf3591c40c9a575ae4f1edc512f9
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-01 18:01:35 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,13 @@
-def 
-
-my_each(collection) do |i|
-  puts i
+def hello_t(array)
+  if block_given?
+    i = 0
+   
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+   array
+  else
+    puts "Hey! No block was given!"
+  end
 end
